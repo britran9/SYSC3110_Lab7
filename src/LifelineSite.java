462ab6d22d5eae83428c508dfb5235574ac7e06a
btran <briantran@cmail.carleton.ca>
2023-11-10 15:41:38 -0500

    Initial Commit

---
+++ src/LifelineSite.java
@@ -0,0 +1,11 @@
+public class LifelineSite extends Site{
+    public LifelineSite(double units, double rate){
+        super(units, rate);
+    }
+
+    public double getBillableAmount(){
+        double base = this.getUnits() * this.getRate() * 0.5;
+        double tax = base * Site.TAX_RATE * 0.2;
+        return base + tax;
+    }
+}
---
+++ src/Site.java
@@ -0,0 +1,19 @@
+public class Site {
+    private double units;
+    private double rate;
+    final public static double TAX_RATE  = 0.13;
+
+    public Site(double units, double rate){
+        this.units = units;
+        this.rate = units;
+    }
+
+    public double getUnits(){
+        return this.units;
+    }
+
+    public double getRate(){
+        return this.rate;
+    }
+
+}
